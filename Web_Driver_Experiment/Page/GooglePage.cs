c01b21b3d7aa26824eddd1fc2e38cdf1f95e8223
Bdruid <fadidib23@hotmail.com>
2023-04-01 17:13:32 +0300

    Fixing .

---
+++ Web_Driver_Experiment/Page/GooglePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+namespace Web_Driver_Experiment.Page
+{
+    public sealed class GooglePage
+    {
+        // Dependency Inversion
+        //Encapsulation of fields and share of functionalities.
+
+        private readonly IWebDriver webDriver;
+        public GooglePage(IWebDriver webDriver) =>
+            this.webDriver = webDriver;
+
+        private IWebElement SearchBar =>
+                this.webDriver.FindElement(By.ClassName(classNameToFind: "gLFyf"));
+
+        private IWebElement SearchButton =>
+            this.webDriver.FindElement(By.ClassName(classNameToFind: "gNO89b"));
+
+        private IWebElement ForestLink =>
+            this.webDriver.FindElement(By.LinkText("Forest - Wikipedia"));
+        public void SearchGoogle(string search)
+        {
+            this.SearchBar.SendKeys(search);
+            this.SearchButton.Submit();
+        }
+
+        public bool isForestTitleDisplay() =>
+            this.ForestLink.Displayed;
+    }
+}
---
+++ Web_Driver_Experiment/Tests/GoogleTest.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Web_Driver_Experiment.Page;
+
+namespace Web_Driver_Experiment.Tests
+{
+    public class GoogleTests
+    {
+        IWebDriver webDriver = new ChromeDriver();
+
+        //Hooks in Nunit
+        //The Setup is that before running any operation run the setup first
+        [SetUp]
+        public void Setup() =>
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+
+        [Test]
+        public void ShouldCheckIfSearchBarWorksWhenClicked()
+        {
+            // Arrange
+            var googlePage = new GooglePage(this.webDriver);
+            
+            // Act
+            googlePage.SearchGoogle("forest");
+
+            // Assert
+            Assert.That(googlePage.isForestTitleDisplay, Is.True);
+        }
+    }
+}
